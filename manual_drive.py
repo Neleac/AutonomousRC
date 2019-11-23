950f9b7feeb2ab71fc1b72728972bb803c2559fd
Caelen Wang <wangc21@uw.edu>
2019-11-22 19:21:38 -0800

    added data collection

--- manual_drive.py
+++ manual_drive.py
@@ -1,8 +1,8 @@
 # Author: Brian Lee <joonl4@uw.edu>
 
-import time, os
+import csv
 import curses
-#import cv2
+import cv2
 import numpy as np
 #from picamera.array import PiRGBArray
 #from picamera import PiCamera
@@ -10,6 +10,11 @@ from pwm_controller import PWMController
     
 
 def main(stdscr):
+    frame_count = -1
+    cap = cv2.VideoCapture(1)
+    csv_file = open('/home/nvidia/Desktop/rc_data/labels.csv', mode='w')
+    csv_writer = csv.writer(csv_file, delimiter=',')
+
     control = PWMController()
     throttle = 0.0
     angle = 0.0
@@ -17,25 +22,29 @@ def main(stdscr):
     stdscr.clear()
     stdscr.nodelay(True)
     while True:
+        frame_count += 1
+        ret, frame = cap.read()
+        cv2.imwrite('/home/nvidia/Desktop/rc_data/images/' + str(frame_count) + '.jpg', frame)
+        csv_writer.writerow([throttle, angle])
+
         txt = stdscr.getch()
-        print(txt)
         if (txt == ord('q')):
             control.steer(0)
             control.drive(0)
             break
         
-        if (txt == curses.KEY_UP):
+        if (txt == curses.KEY_UP and throttle < 0.5):
             throttle += 0.1
-        if (txt == curses.KEY_DOWN):        
+        if (txt == curses.KEY_DOWN and throttle > 0):        
             throttle -= 0.1
-        if (txt == curses.KEY_LEFT):
+        if (txt == curses.KEY_LEFT and angle < 1):
             angle += 0.1
-        if (txt == curses.KEY_RIGHT):
+        if (txt == curses.KEY_RIGHT and angle > -1):
             angle -= 0.1
-        angle = np.clip(angle, -1.0, 1.0)
-        throttle = np.clip(throttle, -1.0, 0.25)
         control.steer(angle)
         control.drive(throttle)
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     curses.wrapper(main)
--- pwm_controller.py
+++ pwm_controller.py
@@ -9,6 +9,7 @@ Abstraction Function:
     PWMController => a pulse-width-modulation motor controller for PCA9685 boards
 '''
 class PWMController:
+    '''
     # RPI
     def __init__(self,
                  address = 0x40,
@@ -21,7 +22,7 @@ class PWMController:
                  min_pulse = 300,
                  zero_pulse = 350):
         self.default_freq = 60
-        '''
+    '''
     # TX2
     def __init__(self,
                  address = 0x40,
@@ -34,7 +35,6 @@ class PWMController:
                  min_pulse = 350,
                  zero_pulse = 300):
         self.default_freq = 50
-        '''
         
         self.pwm_scale = frequency / self.default_freq
         self.pwm = Adafruit_PCA9685.PCA9685(address=address, busnum=busnum)
@@ -85,7 +85,7 @@ class PWMController:
                                 self.left_pulse, self.right_pulse)
 
         self._set_pulse(1, pulse)
-        time.sleep(1)
+        time.sleep(1e-3)
         
 
     def drive(self, throttle):
@@ -105,4 +105,4 @@ class PWMController:
                                     self.min_pulse, self.zero_pulse)
 
         self._set_pulse(0, pulse)
-        time.sleep(1)
+        time.sleep(1e-3)
