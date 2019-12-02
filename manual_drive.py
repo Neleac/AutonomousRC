7e9ca4d9a327fa23c4687d0187f45c4f9edbed9a
Caelen Wang <wangc21@uw.edu>
2019-12-01 21:10:41 -0800

    added realsense support

--- manual_drive.py
+++ manual_drive.py
@@ -1,18 +1,25 @@
-# Author: Brian Lee <joonl4@uw.edu>
+# Authors: Brian Lee <joonl4@uw.edu>
+#          Caelen Wang <wangc21@uw.edu>
 
 import csv
 import curses
 import cv2
 import numpy as np
+import pyrealsense2 as rs
 #from picamera.array import PiRGBArray
 #from picamera import PiCamera
 from pwm_controller import PWMController
-    
+
+DATA_PATH = '/home/nvidia/Desktop/rc_data/'
 
 def main(stdscr):
-    frame_count = -1
+    '''
     cap = cv2.VideoCapture(1)
-    csv_file = open('/home/nvidia/Desktop/rc_data/labels.csv', mode='w')
+    '''
+    pipeline = rs.pipeline()
+    pipeline.start()
+    frame_count = -1
+    csv_file = open(DATA_PATH + 'labels.csv', mode='w')
     csv_writer = csv.writer(csv_file, delimiter=',')
 
     control = PWMController()
@@ -22,9 +29,18 @@ def main(stdscr):
     stdscr.clear()
     stdscr.nodelay(True)
     while True:
-        frame_count += 1
+        '''
         ret, frame = cap.read()
-        cv2.imwrite('/home/nvidia/Desktop/rc_data/images/' + str(frame_count) + '.jpg', frame)
+        '''
+        frame = pipeline.wait_for_frames()
+        img = frame.get_color_frame().as_frame().get_data()
+        img = np.asanyarray(img)
+        depth = frame.get_depth_frame().as_frame().get_data()
+        depth = np.asanyarray(depth)
+
+        frame_count += 1
+        cv2.imwrite(DATA_PATH + 'images/' + str(frame_count) + '.jpg', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        cv2.imwrite(DATA_PATH + 'depth/' + str(frame_count) + '.jpg', depth)
         csv_writer.writerow([throttle, angle])
 
         txt = stdscr.getch()
@@ -37,14 +53,16 @@ def main(stdscr):
             throttle += 0.1
         if (txt == curses.KEY_DOWN and throttle > 0):        
             throttle -= 0.1
-        if (txt == curses.KEY_LEFT and angle < 1):
+        if (txt == curses.KEY_RIGHT and angle < 1):
             angle += 0.1
-        if (txt == curses.KEY_RIGHT and angle > -1):
+        if (txt == curses.KEY_LEFT and angle > -1):
             angle -= 0.1
         control.steer(angle)
         control.drive(throttle)
-    cap.release()
+
+    #cap.release()
     cv2.destroyAllWindows()
+    pipeline.stop()
 
 if __name__ == '__main__':
     curses.wrapper(main)
